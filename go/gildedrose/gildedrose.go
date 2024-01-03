c403e0997efbffde7a84a293eed8c93718a89337
padamk <p.kolhe@f5.com>
2024-01-03 18:12:36 +0530

    refactor with updateQualityAfterSellInChange function

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -32,31 +32,37 @@ func UpdateQuality(items []*Item) {
 			}
 		}
 
-		changeSellIn(items[i])
+		item := items[i]
 
-		if items[i].SellIn < 0 {
-			if items[i].Name != "Aged Brie" {
-				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].Quality > 0 {
-						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-							dropQualityByOne(items[i])
-						}
-					}
-				} else {
-					items[i].Quality = items[i].Quality - items[i].Quality
-				}
-			} else {
-				if items[i].Quality < 50 {
-					incrQualityByOne(items[i])
-				}
-			}
-		}
+		changeSellIn(item)
+
+		updateQualityAfterSellInChange(item)
 	}
 }
 
 func changeSellIn(item *Item) {
 	if item.Name != "Sulfuras, Hand of Ragnaros" {
 		dropSellInByOne(item)
+	}
+}
+
+func updateQualityAfterSellInChange(item *Item) {
+	if item.SellIn < 0 {
+		if item.Name != "Aged Brie" {
+			if item.Name != "Backstage passes to a TAFKAL80ETC concert" {
+				if item.Quality > 0 {
+					if item.Name != "Sulfuras, Hand of Ragnaros" {
+						dropQualityByOne(item)
+					}
+				}
+			} else {
+				item.Quality = item.Quality - item.Quality
+			}
+		} else {
+			if item.Quality < 50 {
+				incrQualityByOne(item)
+			}
+		}
 	}
 }
 
