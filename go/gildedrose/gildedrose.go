7495cd554b0f164de171b81f67739a163878aa3a
padamk <p.kolhe@f5.com>
2024-01-03 18:01:53 +0530

    create incr and decr quality functions

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -11,7 +11,7 @@ func UpdateQuality(items []*Item) {
 		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
 			if items[i].Quality > 0 {
 				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality = items[i].Quality - 1
+					dropQualityByOne(items[i])
 				}
 			}
 		} else {
@@ -20,28 +20,26 @@ func UpdateQuality(items []*Item) {
 				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
 					if items[i].SellIn < 11 {
 						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
+							incrQualityByOne(items[i])
 						}
 					}
 					if items[i].SellIn < 6 {
 						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
+							incrQualityByOne(items[i])
 						}
 					}
 				}
 			}
 		}
 
-		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-			items[i].SellIn = items[i].SellIn - 1
-		}
+		changeSellIn(items[i])
 
 		if items[i].SellIn < 0 {
 			if items[i].Name != "Aged Brie" {
 				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
 					if items[i].Quality > 0 {
 						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-							items[i].Quality = items[i].Quality - 1
+							dropQualityByOne(items[i])
 						}
 					}
 				} else {
@@ -49,10 +47,27 @@ func UpdateQuality(items []*Item) {
 				}
 			} else {
 				if items[i].Quality < 50 {
-					items[i].Quality = items[i].Quality + 1
+					incrQualityByOne(items[i])
 				}
 			}
 		}
 	}
+}
 
+func changeSellIn(item *Item) {
+	if item.Name != "Sulfuras, Hand of Ragnaros" {
+		dropSellInByOne(item)
+	}
+}
+
+func dropQualityByOne(item *Item) {
+	item.Quality = item.Quality - 1
+}
+
+func incrQualityByOne(item *Item) {
+	item.Quality = item.Quality + 1
+}
+
+func dropSellInByOne(item *Item) {
+	item.SellIn = item.SellIn - 1
 }
