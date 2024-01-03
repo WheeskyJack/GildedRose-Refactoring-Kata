d6a0b8e99d74700053bd17e01d1ecc65c8c1b59f
padamk <p.kolhe@f5.com>
2024-01-03 18:07:47 +0530

    add test case till day 6

--- go/gildedrose/gildedrose_test.go
+++ go/gildedrose/gildedrose_test.go
@@ -92,6 +92,48 @@ func getInputs() ([]*gildedrose.Item, []expItems) {
 				{"Conjured Mana Cake", 0, 3},
 			},
 		},
+		{
+			"day 4",
+			[]*gildedrose.Item{
+				{"+5 Dexterity Vest", 6, 16},
+				{"Aged Brie", -2, 6},
+				{"Elixir of the Mongoose", 1, 3},
+				{"Sulfuras, Hand of Ragnaros", 0, 80},
+				{"Sulfuras, Hand of Ragnaros", -1, 80},
+				{"Backstage passes to a TAFKAL80ETC concert", 11, 24},
+				{"Backstage passes to a TAFKAL80ETC concert", 6, 50},
+				{"Backstage passes to a TAFKAL80ETC concert", 1, 50},
+				{"Conjured Mana Cake", -1, 1},
+			},
+		},
+		{
+			"day 5",
+			[]*gildedrose.Item{
+				{"+5 Dexterity Vest", 5, 15},
+				{"Aged Brie", -3, 8},
+				{"Elixir of the Mongoose", 0, 2},
+				{"Sulfuras, Hand of Ragnaros", 0, 80},
+				{"Sulfuras, Hand of Ragnaros", -1, 80},
+				{"Backstage passes to a TAFKAL80ETC concert", 10, 25},
+				{"Backstage passes to a TAFKAL80ETC concert", 5, 50},
+				{"Backstage passes to a TAFKAL80ETC concert", 0, 50},
+				{"Conjured Mana Cake", -2, 0},
+			},
+		},
+		{
+			"day 6",
+			[]*gildedrose.Item{
+				{"+5 Dexterity Vest", 4, 14},
+				{"Aged Brie", -4, 10},
+				{"Elixir of the Mongoose", -1, 0},
+				{"Sulfuras, Hand of Ragnaros", 0, 80},
+				{"Sulfuras, Hand of Ragnaros", -1, 80},
+				{"Backstage passes to a TAFKAL80ETC concert", 9, 27},
+				{"Backstage passes to a TAFKAL80ETC concert", 4, 50},
+				{"Backstage passes to a TAFKAL80ETC concert", -1, 0},
+				{"Conjured Mana Cake", -3, 0},
+			},
+		},
 	}
 	return items, expectedItems
 }
