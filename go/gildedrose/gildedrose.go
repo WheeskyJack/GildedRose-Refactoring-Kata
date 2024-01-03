ee26a5d40d33227060d4d3672f48a94a31520a1b
padamk <p.kolhe@f5.com>
2024-01-03 18:47:08 +0530

    refactor updateQuality with switch case

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -11,32 +11,35 @@ func UpdateQuality(items []*Item) {
 
 		updateSellIn(item) // update SellIn At the end of each day
 
-		updateQualityAfterSellInChange(item) // update Quality based on SellIn has passed expiry At the end of each day
+		updateQualityAfterSellInExpiry(item) // update Quality based on SellIn has passed expiry At the end of each day
 	}
 }
 
 func updateQuality(item *Item) {
-	if item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" {
-		if item.Quality > 0 {
-			if item.Name != "Sulfuras, Hand of Ragnaros" {
-				dropQualityByOne(item)
+	switch item.Name {
+	case "Aged Brie":
+		if item.Quality < 50 {
+			incrQualityByOne(item)
+		}
+	case "Backstage passes to a TAFKAL80ETC concert":
+		if item.Quality < 50 {
+			incrQualityByOne(item)
+			if item.SellIn < 11 {
+				if item.Quality < 50 {
+					incrQualityByOne(item)
+				}
+			}
+			if item.SellIn < 6 {
+				if item.Quality < 50 {
+					incrQualityByOne(item)
+				}
 			}
 		}
-	} else {
-		if item.Quality < 50 {
-			item.Quality = item.Quality + 1
-			if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
-				if item.SellIn < 11 {
-					if item.Quality < 50 {
-						incrQualityByOne(item)
-					}
-				}
-				if item.SellIn < 6 {
-					if item.Quality < 50 {
-						incrQualityByOne(item)
-					}
-				}
-			}
+	case "Sulfuras, Hand of Ragnaros":
+		// do nothing
+	default:
+		if item.Quality > 0 {
+			dropQualityByOne(item)
 		}
 	}
 }
@@ -51,7 +54,7 @@ func updateSellIn(item *Item) {
 	return
 }
 
-func updateQualityAfterSellInChange(item *Item) {
+func updateQualityAfterSellInExpiry(item *Item) {
 	if item.SellIn < 0 {
 		switch item.Name {
 		case "Aged Brie":
